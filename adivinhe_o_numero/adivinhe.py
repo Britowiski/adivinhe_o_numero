2e67871584e80bcef89d13933cf332196b568fcc
Britowiski <132316224+Britowiski@users.noreply.github.com>
2024-03-02 10:35:30 -0300

    Update adivinhe.py

--- adivinhe_o_numero/adivinhe.py
+++ adivinhe_o_numero/adivinhe.py
@@ -1,7 +1,7 @@
 import random
 
 erros = 0
-sorteado = random.randint(0, 100)  # Corrigindo para usar randint que inclui o valor superior
+sorteado = random.randint(0, 100)  
 
 while True:
     jogador = int(input("Tente acertar o número: "))
@@ -9,13 +9,13 @@ while True:
     if sorteado == jogador:
         erros += 1
         print("Parabéns! Você acertou em", erros, "tentativas.")
-        break  # Encerra o loop quando o jogador acerta
+        break  
     elif sorteado > jogador:
         print("ERRO, você errou! O número é maior.")
     else:
         print("ERRO, você errou! O número é menor.")
 
-    erros += 1  # Incrementa o número de erros apenas se o jogador errar
+    erros += 1  
 
-# Fora do loop, após o encerramento do jogo
+
 print("O número sorteado era:", sorteado)
